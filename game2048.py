2266f0394d5369824041e0a2090135af8d68b6fc
David Kaplan <dkaplan@Davids-MacBook-Pro.local>
2018-04-27 17:06:28 -0400

    Made private variables. Created __is_game_over function.

--- game2048.py
+++ game2048.py
@@ -30,26 +30,29 @@ class game2048(object):
              4
         Default size is 4
         '''
-        self.state = np.zeros(shape=(size,size))
-        self.size = size
+        self.__state = np.zeros(shape=(size,size))
+        self.__size = size
         #1: up, 2: down, 3: left, 4: right
         self.action_space = np.array([1,2,3,4])
 
-        self.dodebug = dodebug
+        self.__dodebug = dodebug
 
-        self.did_reset = False
+        self.__did_reset = False
+
+        self.__score = 0
 
         #Probability of 1 tile generated = P_1_new_tile
         #Probability of 2 tiles generated = 1 - P_1_new_tile
-        self.P_1_new_tile = P_1_new_tile
+        self.__P_1_new_tile = P_1_new_tile
         #Probability of tile value 2 is generated = P_val_2_generated
         #Probability of tile value 4 is generated = 1 - P_val_2_generated
-        self.P_val_2_generated = P_val_2_generated
+        self.__P_val_2_generated = P_val_2_generated
 
     def reset(self):
-        self.state = np.zeros(shape=(self.size,self.size))
-        self._add_random_tiles()
-        self.did_reset = True
+        self.__state = np.zeros(shape=(self.__size,self.__size))
+        self.__add_random_tiles()
+        self.__did_reset = True
+        self.__score = 0
 
     def step(self,action):
         '''
@@ -59,67 +62,66 @@ class game2048(object):
          - done (boolean): Did we run out of spots? If yes then return True
          - info (dict): ___TODO___ *****************************
         '''
-        if not self.did_reset:
+        if not self.__did_reset:
             print('Did not reset before taking a step')
             sys.exit()
-        reward = self._move(action)
-
-        done = True if len(np.where(self.states.flatten() == 0)[0]) == 0 else False
-
-        return self.state,reward,done,0
+        reward = self.__move(action)
+        self.__score += reward
+        done = self.__is_game_over()
+        return self.__state,reward,done,0
 
     def render(self):
-        temp = np.zeros(shape=(self.size,self.size))
-        for row in range(self.size):
-            for col in range(self.size):
-                if self.state[row,col] == 0:
+        temp = np.zeros(shape=(self.__size,self.__size))
+        for row in range(self.__size):
+            for col in range(self.__size):
+                if self.__state[row,col] == 0:
                     temp[row,col] = 0
                 else:
-                    temp[row,col] = 2 ** self.state[row,col]
+                    temp[row,col] = 2 ** self.__state[row,col]
         print(np.matrix(temp))
 
-    def _set_state(self,arr):
-        self.state = arr
-
     def get_state(self):
-        return self.state
+        return self.__state
 
-    def get_size(self):
-        return self.size
+    def _set_state(self,arr):
+        '''
+        For Debugging only
+        '''
+        self.__state = arr
 
-    def _move(self,action,add_rand=False):
+    def __move(self,action):
         '''
         Returns the new state and reward after taking action `action`.
-        Uses the function `_condense` to condense each row or column
+        Uses the function `__condense` to condense each row or column
         '''
         cumm_reward = 0
         #up
         if action == 1:
-            for i in range(self.size):
-                self.state[:,i],r = self._condense(self.state[:,i])
+            for i in range(self.__size):
+                self.__state[:,i],r = self.__condense(self.__state[:,i])
                 cumm_reward += r
         #down
         if action == 2:
-            for i in range(self.size):
-                a,r = self._condense(np.fliplr([self.state[:,i]])[0])
-                self.state[:,i] = np.fliplr([a])[0]
+            for i in range(self.__size):
+                a,r = self.__condense(np.fliplr([self.__state[:,i]])[0])
+                self.__state[:,i] = np.fliplr([a])[0]
                 cumm_reward += r
 
         #left
         if action == 3:
-            for i in range(self.size):
-                self.state[i,:],r = self._condense(self.state[i,:])
+            for i in range(self.__size):
+                self.__state[i,:],r = self.__condense(self.__state[i,:])
                 cumm_reward += r
 
         #right
         if action == 4:
-            for i in range(self.size):
-                a,r = self._condense(np.fliplr([self.state[i,:]])[0])
-                self.state[i,:] = np.fliplr([a])[0]
+            for i in range(self.__size):
+                a,r = self.__condense(np.fliplr([self.__state[i,:]])[0])
+                self.__state[i,:] = np.fliplr([a])[0]
                 cumm_reward += r
         return cumm_reward
 
-    def _condense(self,arr):
+    def __condense(self,arr):
         '''
         Input is a 1-dim array of either a column or a row
         [x_1,x_2,...,x_size]
@@ -148,31 +150,53 @@ class game2048(object):
                 ret = np.append(ret,_arr[i])
             i += 1
         # make ret the right length by zero padding
-        if len(ret) < self.size:
-            ret = np.append(ret,np.zeros(self.size-len(ret)))
+        if len(ret) < self.__size:
+            ret = np.append(ret,np.zeros(self.__size-len(ret)))
         return ret,reward
 
-    def _add_random_tiles(self):
+    def __add_random_tiles(self):
         '''
         After you move, new tiles get displayed in the empty space.
         Probabilities of the number of new tiles generated and the values
         that are generated are set in the constructor
         '''
         #temporary state variable
-        _state = self.state.flatten()
+        _state = self.__state.flatten()
         #find indecies where it is empty (value = 0)
         indecies = np.where(_state == 0)[0]
 
         # generate the number of tiles to do
         # check if there are enough spaces
-        num_tiles = 1 if random.random() < self.P_1_new_tile and len(indecies) == 1 else 2
+        num_tiles = 1 if random.random() < self.__P_1_new_tile and len(indecies) == 1 else 2
 
         prev_index = -1
         for i in range(num_tiles):
             #generate value to set
-            val = 1 if random.random() < self.P_val_2_generated else 2
+            val = 1 if random.random() < self.__P_val_2_generated else 2
             #generate index and then set
             ind = indecies[random.randint(0,len(indecies)-1)]
+            #make sure the indecies that are generated are unique
+            while ind == prev_index:
+                ind = indecies[random.randint(0,len(indecies)-1)]
             _state[ind] = val
-        #set self.state to reshaped _state
-        self.state = np.reshape(_state,(self.size,self.size))
+            prev_index = ind
+        #set self.__state to reshaped _state
+        self.__state = np.reshape(_state,(self.__size,self.__size))
+
+    def __is_game_over(self):
+        '''
+        Checks if the game is over.
+        Do this by checking if any reward is returned by `__move` in all directions
+        If the reward > 0, this means that there are places to move and so False is returned
+        '''
+        #check if the board is full
+        if not bool(len(np.where(self.__state == 0)[0])):
+            #record base state
+            _state = self.__state
+            for action in self.action_space:
+                if self.__move(action) > 0:
+                    return False
+                self.__state = _state
+            return True
+        else:
+            return False
